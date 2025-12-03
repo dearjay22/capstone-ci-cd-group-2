ce70d52973746316d40035dd2b76a5fa013bc438
Dhruv7573 <106144101+Dhruv7573@users.noreply.github.com>
2025-12-03 23:45:37 +0000

    Update order service API with get method.

--- orders-service/app.py
+++ orders-service/app.py
@@ -11,3 +11,11 @@ def create_order():
     o["status"] = "created"
     ORDERS.append(o)
     return jsonify(o), 201
+
+@app.route("/orders/user/<int:user_id>", methods=["GET"])
+def get_orders_for_user(user_id):
+    res = [o for o in ORDERS if o.get("user_id")==user_id]
+    return jsonify(res), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5003)    
